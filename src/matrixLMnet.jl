34c0d74f08aa75035b9291b336147bb9c74475f8
Jane Liang <janewliang@users.noreply.github.com>
2019-11-16 18:02:32 -0500

    Export Mlmnet_cv objects

--- src/matrixLMnet.jl
+++ src/matrixLMnet.jl
@@ -21,7 +21,7 @@ export Response, Predictors, RawData, get_X, get_Z, get_Y, contr,
     cd!, cd_active!, ista!, fista!, fista_bt!, admm!, 
     Mlmnet, mlmnet, coef, coef_2d, predict, fitted, resid, 
     mlmnet_perms, 
-    make_folds, make_folds_conds, mlmnet_cv, 
+    make_folds, make_folds_conds, Mlmnet_cv, mlmnet_cv, 
     avg_mse, lambda_min, avg_prop_zero, mlmnet_cv_summary
 
 
