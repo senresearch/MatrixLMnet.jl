e35c3f1ce876b5d30d69f8c0f5e4402884079426
Gregory Farage <faragegr@gmail.com>
2025-11-12 06:27:59 -0600

    updated min_rows function and tests

--- test/mlmnetCV_helpersTests.jl
+++ test/mlmnetCV_helpersTests.jl
@@ -18,14 +18,14 @@
     @test MatrixLMnet.minimize_rows([CartesianIndex(3, 1), CartesianIndex(2, 1)]) == [CartesianIndex(2, 1)]
 
     # Test with multiple elements in different columns
-    @test MatrixLMnet.minimize_rows([CartesianIndex(1, 1), CartesianIndex(2, 2), CartesianIndex(3, 2)]) == [CartesianIndex(2, 2), CartesianIndex(1, 1)]
+    @test MatrixLMnet.minimize_rows([CartesianIndex(1, 1), CartesianIndex(2, 2), CartesianIndex(3, 2)]) == [CartesianIndex(1, 1), CartesianIndex(2, 2)]
 
     # Test with non-sequential columns
-    @test MatrixLMnet.minimize_rows([CartesianIndex(3, 5), CartesianIndex(2, 1)]) == [CartesianIndex(3, 5), CartesianIndex(2, 1)]
+    @test MatrixLMnet.minimize_rows([CartesianIndex(3, 5), CartesianIndex(2, 1)]) == [CartesianIndex(2, 1), CartesianIndex(3, 5)]
 
     # Test with duplicate indices
     @test MatrixLMnet.minimize_rows([CartesianIndex(2, 2), CartesianIndex(2, 2)]) == [CartesianIndex(2, 2)]
-
+    
     # Test with a more complex scenario
-    @test MatrixLMnet.minimize_rows([CartesianIndex(4, 3), CartesianIndex(1, 3), CartesianIndex(2, 4), CartesianIndex(3, 4)]) == [CartesianIndex(2, 4), CartesianIndex(1, 3)]
+    @test MatrixLMnet.minimize_rows([CartesianIndex(4, 3), CartesianIndex(1, 3), CartesianIndex(2, 4), CartesianIndex(3, 4)]) == [CartesianIndex(1, 3), CartesianIndex(2, 4)]
 end
